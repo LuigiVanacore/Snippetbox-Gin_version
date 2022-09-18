5458b4222cbcc906987660ce09eb3469f6242c9a
Luigi Vanacore <luigi.vanacore@relatech.com>
2022-09-18 16:02:10 +0200

    added snippetView and snippetCreate dummy handlers

--- main.go
+++ main.go
@@ -5,17 +5,27 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, _ *http.Request) {
-	_, err := w.Write([]byte("Hello from Snippetbox"))
-	if err != nil {
-		return
-	}
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
-
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
